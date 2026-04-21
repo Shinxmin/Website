0f305418be8de889e1f0f96782d99f8c89a0a3de
Shinxmin <143274157+Shinxmin@users.noreply.github.com>
2026-04-21 22:18:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function changeTab(tab){
-  // 텍스트 변경
+function changeTab(tab, el){
+  // 콘텐츠 변경
   document.getElementById("content").innerText = tab + " 화면";
 
-  // active 상태 변경
-  document.querySelectorAll(".tab").forEach(el => {
-    el.classList.remove("active");
+  // active 제거
+  document.querySelectorAll(".tab").forEach(t => {
+    t.classList.remove("active");
   });
 
-  event.target.classList.add("active");
+  // 클릭한 요소에 active
+  el.classList.add("active");
 }
